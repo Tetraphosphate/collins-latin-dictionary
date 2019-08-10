09f4c24602f99dc215e45e979d72ee80aa2bca9e
Janice <janice.c.chung@gmail.com>
2019-08-11 11:59:48 +1200

    Create data.js

---
+++ data.js
@@ -0,0 +1,956 @@
+var json = `
+
+[
+  {
+    "id": 0,
+    "name": "Woden",
+    "mother": "",
+    "father": "",
+    "class": "legendary saxon male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 1,
+    "name": "Bældæg",
+    "mother": "",
+    "father": 0,
+    "class": "legendary saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 2,
+    "name": "Brond",
+    "mother": "",
+    "father": 1,
+    "class": "legendary saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 3,
+    "name": "Freawine",
+    "mother": "",
+    "father": 2,
+    "class": "legendary saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 4,
+    "name": "Wig",
+    "mother": "",
+    "father": 3,
+    "class": "legendary saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 5,
+    "name": "Giwis",
+    "mother": "",
+    "father": 4,
+    "class": "legendary saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 6,
+    "name": "Elesa",
+    "mother": "",
+    "father": 5,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 7,
+    "name": "Cerdic",
+    "mother": "",
+    "father": 6,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 534,
+    "spouse": ""
+  },
+  {
+    "id": 8,
+    "name": "Cynric",
+    "mother": "",
+    "father": 7,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 560,
+    "spouse": ""
+  },
+  {
+    "id": 9,
+    "name": "Ceawlin",
+    "mother": "",
+    "father": 8,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 593,
+    "spouse": ""
+  },
+  {
+    "id": 10,
+    "name": "Cuthwulf",
+    "mother": "",
+    "father": 8,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 11,
+    "name": "Stuf",
+    "mother": "",
+    "father": 6,
+    "class": "wihtwara jutish male ruler descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 12,
+    "name": "Wihtgar",
+    "mother": "",
+    "father": 6,
+    "class": "wihtwara jutish male ruler descent",
+    "birthday": "",
+    "deathday": 544,
+    "spouse": ""
+  },
+  {
+    "id": 13,
+    "name": "Cuthwine",
+    "mother": "",
+    "father": 9,
+    "class": "male",
+    "birthday": 565,
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 14,
+    "name": "Cutha",
+    "mother": "",
+    "father": 9,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 15,
+    "name": "Clemen ap Bledric",
+    "mother": "",
+    "father": "",
+    "class": "dumnonia welsh male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 16,
+    "name": "Cynebald",
+    "mother": "",
+    "father": 13,
+    "class": "male",
+    "birthday": 585,
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 17,
+    "name": "Cedda",
+    "mother": "",
+    "father": 13,
+    "class": "male",
+    "birthday": 590,
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 18,
+    "name": "Cuthwulf",
+    "mother": "",
+    "father": 13,
+    "class": "male",
+    "birthday": 592,
+    "deathday": 648,
+    "spouse": 19
+  },
+  {
+    "id": 19,
+    "name": "Gwynhafar",
+    "mother": "",
+    "father": 15,
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 18
+  },
+  {
+    "id": 20,
+    "name": "Cēnfūs",
+    "mother": "",
+    "father": 8,
+    "class": "wessex anglo-saxon male ruler descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 21,
+    "name": "Æscwine",
+    "mother": "",
+    "father": 20,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 676,
+    "spouse": ""
+  },
+  {
+    "id": 22,
+    "name": "Cēnberht",
+    "mother": "",
+    "father": 17,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 661,
+    "spouse": ""
+  },
+  {
+    "id": 23,
+    "name": "Ceowald",
+    "mother": 19,
+    "father": 18,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 24,
+    "name": "Cædwalla",
+    "mother": "",
+    "father": 22,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": 659,
+    "deathday": "20-04-689",
+    "spouse": 25
+  },
+  {
+    "id": 25,
+    "name": "Cynethryth",
+    "mother": "",
+    "father": "",
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 24
+  },
+  {
+    "id": 26,
+    "name": "Mūl",
+    "mother": "",
+    "father": 22,
+    "class": "kent anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 687,
+    "spouse": ""
+  },
+  {
+    "id": 27,
+    "name": "Cenred",
+    "mother": "",
+    "father": 23,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 28,
+    "name": "Ine",
+    "mother": "",
+    "father": 27,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": 670,
+    "deathday": 726,
+    "spouse": 29
+  },
+  {
+    "id": 29,
+    "name": "Æthelburg",
+    "mother": "",
+    "father": 30,
+    "class": "female",
+    "birthday": 673,
+    "deathday": 740,
+    "spouse": 28
+  },
+  {
+    "id": 30,
+    "name": "Unknown",
+    "mother": "",
+    "father": 8,
+    "class": "unknown descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 31,
+    "name": "Arwald",
+    "mother": "",
+    "father": 12,
+    "class": "wihtwara jutish male ruler descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 32,
+    "name": "2 sons",
+    "mother": "",
+    "father": 31,
+    "class": "unknown",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 33,
+    "name": "Ingild",
+    "mother": "",
+    "father": 27,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 34,
+    "name": "Cwenburh",
+    "mother": "",
+    "father": 27,
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 35,
+    "name": "Cynric the Æthling",
+    "mother": "",
+    "father": 8,
+    "class": "wessex anglo-saxon male descent",
+    "birthday": "",
+    "deathday": 748,
+    "spouse": ""
+  },
+  {
+    "id": 36,
+    "name": "Æþelheard",
+    "mother": "",
+    "father": 30,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 740,
+    "spouse": 37
+  },
+  {
+    "id": 37,
+    "name": "Frithugyth",
+    "mother": "",
+    "father": "",
+    "class": "female",
+    "birthday": "",
+    "deathday": 737,
+    "spouse": 36
+  },
+  {
+    "id": 38,
+    "name": "Cuthred",
+    "mother": "",
+    "father": 30,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 756,
+    "spouse": ""
+  },
+  {
+    "id": 39,
+    "name": "Cynric",
+    "mother": "",
+    "father": 38,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 40,
+    "name": "Sigeberht",
+    "mother": "",
+    "father": 41,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 41,
+    "name": "Unknown",
+    "mother": "",
+    "father": 8,
+    "class": "unknown descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 42,
+    "name": "Cyneheard",
+    "mother": "",
+    "father": 41,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 786,
+    "spouse": ""
+  },
+  {
+    "id": 43,
+    "name": "Cūþburh",
+    "mother": "",
+    "father": 27,
+    "class": "female",
+    "birthday": "",
+    "deathday": 718,
+    "spouse": 44
+  },
+  {
+    "id": 44,
+    "name": "Aldfrith",
+    "mother": "",
+    "father": "",
+    "class": "northumbria anglo-saxon male ruler",
+    "birthday": 685,
+    "deathday": 705,
+    "spouse": 43
+  },
+  {
+    "id": 45,
+    "name": "Eoppa",
+    "mother": "",
+    "father": 33,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 46,
+    "name": "Eafa",
+    "mother": "",
+    "father": 45,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 47,
+    "name": "Eahlmund",
+    "mother": "",
+    "father": 46,
+    "class": "kent anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 784,
+    "spouse": 60
+  },
+  {
+    "id": 48,
+    "name": "Ceol",
+    "mother": "",
+    "father": 10,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 597,
+    "spouse": ""
+  },
+  {
+    "id": 49,
+    "name": "Cynegils",
+    "mother": "",
+    "father": 48,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 642,
+    "spouse": ""
+  },
+  {
+    "id": 50,
+    "name": "Cwichelm",
+    "mother": "",
+    "father": 49,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 636,
+    "spouse": ""
+  },
+  {
+    "id": 51,
+    "name": "Centwine",
+    "mother": "",
+    "father": 49,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 685,
+    "spouse": ""
+  },
+  {
+    "id": 52,
+    "name": "Unknown",
+    "mother": "",
+    "father": 53,
+    "class": "unknown female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 54
+  },
+  {
+    "id": 53,
+    "name": "Pybba",
+    "mother": "",
+    "father": "",
+    "class": "mercia anglo-saxon male ruler",
+    "birthday": 570,
+    "deathday": 615,
+    "spouse": ""
+  },
+  {
+    "id": 54,
+    "name": "Cenwalh",
+    "mother": "",
+    "father": 49,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 672,
+    "spouse": "52 55"
+  },
+  {
+    "id": 55,
+    "name": "Seaxburh",
+    "mother": "",
+    "father": "",
+    "class": "wessex anglo-saxon female ruler",
+    "birthday": "",
+    "deathday": 674,
+    "spouse": 54
+  },
+  {
+    "id": 56,
+    "name": "Kyneburga",
+    "mother": "",
+    "father": 49,
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 57,
+    "name": "Æðelberht II",
+    "mother": "",
+    "father": "",
+    "class": "kent anglo-saxon male ruler",
+    "birthday": 725,
+    "deathday": 762,
+    "spouse": ""
+  },
+  {
+    "id": 58,
+    "name": "Oslac",
+    "mother": "",
+    "father": 12,
+    "class": "male descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 59,
+    "name": "Ecgberht",
+    "mother": 60,
+    "father": 47,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": 771,
+    "deathday": 639,
+    "spouse": ""
+  },
+  {
+    "id": 60,
+    "name": "Unknown",
+    "mother": "",
+    "father": 57,
+    "class": "unknown female descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 47
+  },
+  {
+    "id": 61,
+    "name": "Beorhtric",
+    "mother": "",
+    "father": "",
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 802,
+    "spouse": 62
+  },
+  {
+    "id": 62,
+    "name": "Ēadburh",
+    "mother": "",
+    "father": "",
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 61
+  },
+  {
+    "id": 63,
+    "name": "Osburh",
+    "mother": "",
+    "father": 58,
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 64
+  },
+  {
+    "id": 64,
+    "name": "Æðelwulf",
+    "mother": "",
+    "father": 59,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": "13-01-858",
+    "spouse": "63 65"
+  },
+  {
+    "id": 65,
+    "name": "Judith of Flanders",
+    "mother": "",
+    "father": "",
+    "class": "female",
+    "birthday": 843,
+    "deathday": 870,
+    "spouse": "64 67"
+  },
+  {
+    "id": 66,
+    "name": "Æthelstan",
+    "mother": 63,
+    "father": 64,
+    "class": "kent anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 852,
+    "spouse": ""
+  },
+  {
+    "id": 67,
+    "name": "Æthelbald",
+    "mother": 63,
+    "father": 64,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 860,
+    "spouse": 65
+  },
+  {
+    "id": 68,
+    "name": "Æthelberht",
+    "mother": 63,
+    "father": 64,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 865,
+    "spouse": ""
+  },
+  {
+    "id": 69,
+    "name": "Burgred",
+    "mother": "",
+    "father": "",
+    "class": "mercia anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 70
+  },
+  {
+    "id": 70,
+    "name": "Æthelswith",
+    "mother": 63,
+    "father": 64,
+    "class": "female",
+    "birthday": 838,
+    "deathday": 888,
+    "spouse": 69
+  },
+  {
+    "id": 71,
+    "name": "Æþelræd I",
+    "mother": 63,
+    "father": 64,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": 847,
+    "deathday": 871,
+    "spouse": 72
+  },
+  {
+    "id": 72,
+    "name": "Wulfthryth",
+    "mother": "",
+    "father": "",
+    "class": "female",
+    "birthday": "",
+    "deathday": 868,
+    "spouse": 71
+  },
+  {
+    "id": 73,
+    "name": "Æþelhelm",
+    "mother": 72,
+    "father": 71,
+    "class": "male",
+    "birthday": "",
+    "deathday": 880,
+    "spouse": ""
+  },
+  {
+    "id": 74,
+    "name": "Æthelwold",
+    "mother": 72,
+    "father": 71,
+    "class": "male",
+    "birthday": "",
+    "deathday": 903,
+    "spouse": ""
+  },
+  {
+    "id": 75,
+    "name": "Ælfrēd the Great",
+    "mother": 63,
+    "father": 64,
+    "class": "wessex anglo-saxon male ruler",
+    "birthday": 848,
+    "deathday": "26-10-899",
+    "spouse": 76
+  },
+  {
+    "id": 76,
+    "name": "Eahlswith",
+    "mother": 77,
+    "father": 78,
+    "class": "female",
+    "birthday": "",
+    "deathday": 902,
+    "spouse": 75
+  },
+  {
+    "id": 77,
+    "name": "Eadburh",
+    "mother": "",
+    "father": 79,
+    "class": "female descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 78
+  },
+  {
+    "id": 78,
+    "name": "Æthelred Mucel",
+    "mother": "",
+    "father": "",
+    "class": "mercia anglo-saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 77
+  },
+  {
+    "id": 79,
+    "name": "Coenwulf",
+    "mother": "",
+    "father": "",
+    "class": "mercia anglo-saxon male ruler",
+    "birthday": "",
+    "deathday": 821,
+    "spouse": ""
+  },
+  {
+    "id": 80,
+    "name": "Æthelwulf",
+    "mother": 77,
+    "father": 78,
+    "class": "mercia anglo-saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 81,
+    "name": "Æthelgyth",
+    "mother": "",
+    "father": 80,
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 82
+  },
+  {
+    "id": 82,
+    "name": "Æthelfrith",
+    "mother": "",
+    "father": "",
+    "class": "mercia anglo-saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": 81
+  },
+  {
+    "id": 83,
+    "name": "Ælfstan",
+    "mother": 81,
+    "father": 82,
+    "class": "mercia anglo-saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 84,
+    "name": "Æthelstan Half-King",
+    "mother": 81,
+    "father": 82,
+    "class": "east-anglia anglo-saxon male",
+    "birthday": 932,
+    "deathday": 956,
+    "spouse": ""
+  },
+  {
+    "id": 85,
+    "name": "Æthelwald",
+    "mother": 81,
+    "father": 82,
+    "class": "kent anglo-saxon male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 86,
+    "name": "Eadric",
+    "mother": 81,
+    "father": 82,
+    "class": "wessex anglo-saxon male",
+    "birthday": "",
+    "deathday": 949,
+    "spouse": ""
+  },
+  {
+    "id": 87,
+    "name": "Unknown",
+    "mother": "",
+    "father": 74,
+    "class": "unknown descent",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 88,
+    "name": "Æthelweard the Historian",
+    "mother": "",
+    "father": 87,
+    "class": "male",
+    "birthday": "",
+    "deathday": 998,
+    "spouse": ""
+  },
+  {
+    "id": 89,
+    "name": "Ælfweard",
+    "mother": "",
+    "father": 87,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 90,
+    "name": "Ælfwaru",
+    "mother": "",
+    "father": 87,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 91,
+    "name": "Ælfgifu",
+    "mother": "",
+    "father": 87,
+    "class": "female",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  },
+  {
+    "id": 92,
+    "name": "Æthelmær Cild",
+    "mother": "",
+    "father": 88,
+    "class": "male",
+    "birthday": "",
+    "deathday": 1015,
+    "spouse": ""
+  },
+  {
+    "id": 93,
+    "name": "Æthelnoth",
+    "mother": "",
+    "father": 92,
+    "class": "male",
+    "birthday": "",
+    "deathday": 1038,
+    "spouse": ""
+  },
+  {
+    "id": 94,
+    "name": "Æthelweard",
+    "mother": "",
+    "father": 92,
+    "class": "male",
+    "birthday": "",
+    "deathday": "",
+    "spouse": ""
+  }
+]
+
+`;
